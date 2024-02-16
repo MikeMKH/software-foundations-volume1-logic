6080d1b6acc3cfde72a8b1ceba70ab42af3d23a0
Mike Harris <MikeMKH@users.noreply.github.com>
2024-02-16 06:02:22 -0600

    add warmups

--- basic.v
+++ basic.v
@@ -413,3 +413,35 @@ Fixpoint plus' (n : nat) (m : nat) : nat :=
 Compute plus' 3 4.
 Compute plus' 0 5.
 Compute plus' 8 0.
+
+Theorem identity_fn_applied_twice:
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = x) -> forall (b : bool),
+  f (f b) = b.
+Proof.
+  intros f H b.
+  rewrite -> H.
+  rewrite -> H.
+  reflexivity.
+Qed.
+
+Theorem negation_fn_applied_twice:
+  forall (f : bool -> bool),
+  (forall (x : bool), f x = negb x) -> forall (b : bool),
+  f (f b) = b.
+Proof.
+  intros f H b.
+  rewrite -> H.
+  rewrite -> H.
+  rewrite -> negb_involutive.
+  reflexivity.
+Qed.
+
+Theorem andb_eq_orb:
+  forall (b c : bool), (andb b c = orb b c) -> b = c.
+Proof.
+  intros b c H.
+  destruct b.
+  - apply eq_sym; trivial.
+  - assumption.
+Qed.
