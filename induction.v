441d6cfde4cb1bdb2841f76e76dd864b07a57163
Mike Harris <MikeMKH@users.noreply.github.com>
2024-02-29 06:07:32 -0600

    add formal vs informal proofs

--- induction.v
+++ induction.v
@@ -126,3 +126,46 @@ Proof.
   rewrite <- H.
   reflexivity.
 Qed.
+
+Theorem add_assoc' :
+  forall n m p : nat, n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p. induction n as [|n' IHn'].
+  - (* n = 0 *)
+    simpl; reflexivity.
+  - (* n = S n' *)
+    {
+      simpl; rewrite IHn'.
+      reflexivity.
+    }
+Qed.
+
+Theorem add_comm' :
+  forall n m : nat, n + m = m + n.
+Proof.
+  intros n m. induction m as [|m' IHm'].
+  - (* m = 0 *)
+    {
+      simpl; rewrite -> add_0_r.
+      reflexivity.
+    }
+  - (* m = S m' *)
+    {
+      simpl; rewrite <- IHm'.
+      rewrite <- plus_n_Sm.
+      reflexivity.
+    }
+Qed.
+
+Theorem eqb_refl' :
+  forall n : nat, (n =? n) = true.
+Proof.
+  intros n. induction n as [|n' IHn'].
+  - (* n = 0 *)
+    simpl; reflexivity.
+  - (* n = S n' *)
+    {
+      simpl. rewrite -> IHn'.
+      reflexivity.
+    }
+Qed.
